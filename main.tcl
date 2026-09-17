c695d9da340adf218b9770711aae1694f8583a66
srhodus <s.rhodus@yahoo.com>
2026-09-17 09:32:07 -0500

    Fix initial click to be safe, add colored bomb indicators

--- main.tcl
+++ main.tcl
@@ -1,4 +1,4 @@
-#!/usr/bin/tclsh
+#!/usr/bin/env tclsh
 
 package require Tk
 
@@ -68,6 +68,7 @@ proc reset_game {} {
             
             set b [button .board.b_${r}_${c} -width 2 -height 1 \
                     -font {Helvetica 11 bold} -relief raised \
+                    -bg "#d9d9d9" \
                     -command [list click_cell $r $c]]
             
             # Right-click / Ctrl-click: Toggle Flag
@@ -83,20 +84,36 @@ proc reset_game {} {
     }
 }
 
+proc is_safe_zone {r c safe_r safe_c} {
+    # Simon Tatham guarantee: Clicked cell AND all 8 adjacent neighbors must be bomb-free
+    return [expr {abs($r - $safe_r) <= 1 && abs($c - $safe_c) <= 1}]
+}
+
 proc place_bombs {safe_r safe_c} {
     global ROWS COLS BOMBS board
 
+    # Max bombs safety check to avoid infinite loop on small boards
+    set safe_zone_size 0
+    for {set r 0} {$r < $ROWS} {incr r} {
+        for {set c 0} {$c < $COLS} {incr c} {
+            if {[is_safe_zone $r $c $safe_r $safe_c]} { incr safe_zone_size }
+        }
+    }
+    set max_allowed [expr {$ROWS * $COLS - $safe_zone_size}]
+    set actual_bombs [expr {$BOMBS > $max_allowed ? $max_allowed : $BOMBS}]
+
     set placed 0
-    while {$placed < $BOMBS} {
+    while {$placed < $actual_bombs} {
         set r [expr {int(rand() * $ROWS)}]
         set c [expr {int(rand() * $COLS)}]
         
-        if {$board($r,$c) ne "B" && !($r == $safe_r && $c == $safe_c)} {
+        if {$board($r,$c) ne "B" && ![is_safe_zone $r $c $safe_r $safe_c]} {
             set board($r,$c) "B"
             incr placed
         }
     }
 
+    # Calculate neighbor numbers
     for {set r 0} {$r < $ROWS} {incr r} {
         for {set c 0} {$c < $COLS} {incr c} {
             if {$board($r,$c) eq "B"} continue
@@ -124,15 +141,13 @@ proc start_timer {} {
 proc click_cell {r c} {
     global game_started game_over state board btn NUM_COLORS revealed_count ROWS COLS BOMBS
 
-    if {$game_over || $state($r,$c) eq "flagged"} return
+    if {$game_over || $state($r,$c) eq "flagged" || $state($r,$c) eq "revealed"} return
 
     if {!$game_started} {
         set game_started 1
         place_bombs $r $c
         start_timer
     }
-
-    if {$state($r,$c) eq "revealed"} return
 
     set state($r,$c) "revealed"
     incr revealed_count
@@ -145,10 +160,11 @@ proc click_cell {r c} {
 
     if {$board($r,$c) > 0} {
         set val $board($r,$c)
-        $btn($r,$c) configure -text $val -fg $NUM_COLORS($val) -relief sunken -state disabled
+        $btn($r,$c) configure -text $val -fg $NUM_COLORS($val) -relief sunken -bg "#e0e0e0"
     } else {
-        $btn($r,$c) configure -text "" -relief sunken -state disabled -bg "#e0e0e0"
+        $btn($r,$c) configure -text "" -relief sunken -bg "#e0e0e0"
         
+        # Recursive flood fill across zero-value tiles
         foreach {dr dc} {-1 -1 -1 0 -1 1 0 -1 0 1 1 -1 1 0 1 1} {
             set nr [expr {$r + $dr}]
             set nc [expr {$c + $dc}]
@@ -170,7 +186,6 @@ proc chord_cell {r c} {
 
     if {$game_over || $state($r,$c) ne "revealed" || $board($r,$c) <= 0} return
 
-    # Count adjacent flags
     set flag_count 0
     set neighbors {}
     
@@ -186,7 +201,6 @@ proc chord_cell {r c} {
         }
     }
 
-    # If flags match tile value, reveal remaining unflagged neighbors
     if {$flag_count == $board($r,$c)} {
         foreach pos $neighbors {
             lassign $pos nr nc
@@ -232,7 +246,6 @@ proc game_win {} {
     set flags_left 0
     .top.reset configure -text "😎"
 
-    # Automatically flag all remaining unrevealed bombs
     for {set r 0} {$r < $ROWS} {incr r} {
         for {set c 0} {$c < $COLS} {incr c} {
             if {$board($r,$c) eq "B" && $state($r,$c) ne "flagged"} {
